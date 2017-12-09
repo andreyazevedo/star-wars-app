d637d00d71a9d927656aaa1e03b1bafef4197ba5
Andrey Azevedo <andreyaze@gmail.com>
2017-12-09 19:18:32 -0200

    test Planet component render

---
+++ src/Planet.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Planet from './Planet';
+
+it('component Planet renders without crashing', () => {
+  const fakeData = {
+    name: 'Lorem',
+    population: 9000,
+    climate: 'tropical',
+    terrain: 'desert',
+    films: []
+  };
+  const div = document.createElement('div');
+  ReactDOM.render(<Planet data={ fakeData } />, div);
+});
